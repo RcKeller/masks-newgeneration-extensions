87b5c39c9e4fce7be04d279ad5a3cffee139bb3d
Andy Keller <andy@barrfitz.com>
2025-11-05 19:04:48 -0700

    feat: save wip

--- module/masks-extensions.mjs
+++ module/masks-extensions.mjs
@@ -5,3 +5,16 @@ import './xcard.mjs'
 import './advantage.mjs'
 import './encounter-tracker.mjs'
 import './conditions.mjs'
+import './health.mjs'
+// import { configSheet } from "./helpers/config-sheet.mjs";
+// import { MasksActorSheetMixin } from './sheets/actor-sheet.mjs';
+
+// Hooks.once("init", () => {
+//     const masksActorSheet = MasksActorSheetMixin(game.pbta.applications.actor.PbtaActorSheet);
+//     Actors.unregisterSheet('pbta', game.pbta.applications.actor.PbtaActorSheet, { types: ['character'] });
+//     Actors.registerSheet('pbta', masksActorSheet, {
+//         types: ['character'],
+//         makeDefault: true,
+//         label: 'MASKS-SHEETS.SheetConfig.character',
+//     });
+// })
